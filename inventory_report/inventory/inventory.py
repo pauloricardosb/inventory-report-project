b5cd91155db04a3c70e38e0a67ceefa8dbe4e260
Paulo Ricardo <pauloricardosilvabarboza@outlook.com>
2023-02-03 14:53:07 -0300

    FEAT: Import JSON

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,5 @@
 import csv
+import json
 
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
@@ -11,6 +12,9 @@ class Inventory:
             if path.endswith(".csv"):
                 reader = csv.DictReader(file)
                 data = list(reader)
+
+            elif path.endswith(".json"):
+                data = json.load(file)
 
         if type == "simples":
             return SimpleReport.generate(data)
