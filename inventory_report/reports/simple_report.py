d677b6025944d8fb81b6334bfc641ca34afcc0d4
Paulo Ricardo <pauloricardosilvabarboza@outlook.com>
2023-02-03 14:32:02 -0300

    FEAT: Create Simple Report class

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,28 @@
+from datetime import date
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    @classmethod
+    def generate(cls, inventory):
+        dates = [
+            product["data_de_fabricacao"] for product in inventory
+        ]
+        oldest_dates = sorted(dates)[0]
+
+        today_date = str(date.today())
+        expiration_dates = [
+            product["data_de_validade"]
+            for product in inventory
+            if product["data_de_validade"] >= today_date
+        ]
+        closest_dates = sorted(expiration_dates)[0]
+
+        companies = [product["nome_da_empresa"] for product in inventory]
+        companie_most_products = Counter(companies).most_common()[0][0]
+
+        return (
+            f"Data de fabricação mais antiga: {oldest_dates}\n"
+            f"Data de validade mais próxima: {closest_dates}\n"
+            f"Empresa com mais produtos: {companie_most_products}"
+        )
