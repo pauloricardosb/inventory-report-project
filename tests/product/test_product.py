2a2389d1814f8f61054785d325822023d6240f73
Paulo Ricardo <pauloricardosilvabarboza@outlook.com>
2023-02-03 13:54:22 -0300

    FEAT: Create a Product test

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "Coca-Cola",
+        "Coca-Cola Company",
+        "2020-01-01",
+        "2020-12-31",
+        "123456789",
+        "em local seco",
+    )
+    assert product.id == 1
+    assert product.nome_do_produto == "Coca-Cola"
+    assert product.nome_da_empresa == "Coca-Cola Company"
+    assert product.data_de_fabricacao == "2020-01-01"
+    assert product.data_de_validade == "2020-12-31"
+    assert product.numero_de_serie == "123456789"
+    assert product.instrucoes_de_armazenamento == "em local seco"
