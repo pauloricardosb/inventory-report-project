374ea8eab206178ed63c2d636fbc103bac7ace6c
Paulo Ricardo <pauloricardosilvabarboza@outlook.com>
2023-02-03 14:36:00 -0300

    FEAT: Create Complete Report class

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,16 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    @classmethod
+    def generate(cls, data):
+        report = super().generate(data)
+
+        companies_counter = cls.companies_counter(data)
+
+        report += "\nProdutos estocados por empresa:\n"
+
+        for company, quantity in companies_counter.items():
+            report += f"- {company}: {quantity}\n"
+
+        return report
--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -4,10 +4,13 @@ from collections import Counter
 
 class SimpleReport:
     @classmethod
+    def companies_counter(cls, inventory):
+        companies = [product["nome_da_empresa"] for product in inventory]
+        return Counter(companies)
+
+    @classmethod
     def generate(cls, inventory):
-        dates = [
-            product["data_de_fabricacao"] for product in inventory
-        ]
+        dates = [product["data_de_fabricacao"] for product in inventory]
         oldest_dates = sorted(dates)[0]
 
         today_date = str(date.today())
@@ -18,8 +21,9 @@ class SimpleReport:
         ]
         closest_dates = sorted(expiration_dates)[0]
 
-        companies = [product["nome_da_empresa"] for product in inventory]
-        companie_most_products = Counter(companies).most_common()[0][0]
+        companie_most_products = cls.companies_counter(
+            inventory
+        ).most_common()[0][0]
 
         return (
             f"Data de fabricação mais antiga: {oldest_dates}\n"
